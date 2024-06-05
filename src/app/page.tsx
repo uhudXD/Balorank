ee0ad9ddfc5c9188fcd2217783769306bf89d209
uhudXD <uhud.trb@gmail.com>
2024-06-06 01:20:58 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
         <a
           href="/Agents"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-100 hover:bg-muted hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
